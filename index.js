62e18990b2d270336d734a9cb5a26d4fa3b34da7
Carl <carlbrainegebe@gmail.com>
2021-05-20 23:15:27 -0500

    commit tree 3501995ae54c63ddc0a70dcda7b13c3c6a56e561

--- index.js
+++ index.js
@@ -1,46 +1,111 @@
-// Write your solution here!
 const cats = ["Milo", "Otis", "Garfield"];
 
-
- function destructivelyAppendCat(name) {
-   
-  cats.push(name);
+function destructivelyAppendCat(name) {
+cats.push(name);
 }
 
 function destructivelyPrependCat(name) {
-
-  cats.unshift(name);
+cats.unshift(name);
 }
 
 function destructivelyRemoveLastCat() {
-
   cats.pop();
 }
 
 function destructivelyRemoveFirstCat() {
-
   cats.shift();
 }
 
 function appendCat(name) {
-  const newArray = [...cats.slice(), name];
-  return newArray;
+  const newCatArray = [...cats, name];
+
+  return newCatArray;
 }
 
 function prependCat(name) {
+  const newCatArray = [name, ...cats];
 
-  const newArray = [name, ...cats];
-  return newArray;
+  return newCatArray;
 }
 
 function removeLastCat() {
+const newCatArray = cats.slice(0,-1);
 
+return newCatArray;
 
-  const newArray = [...cats.slice(0, cats.length -1)];
-  return newArray;
 }
 
- function removeFirstCat() {
-  const newArray = [...cats.slice(1)];
-  return newArray;
- }
+function removeFirstCat() {
+const newArray = cats.slice(1);
+
+return newArray;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Write your solution here!
+// const cats = ["Milo", "Otis", "Garfield"];
+
+
+//  function destructivelyAppendCat(name) {
+   
+//   cats.push(name);
+// }
+
+// function destructivelyPrependCat(name) {
+
+//   cats.unshift(name);
+// }
+
+// function destructivelyRemoveLastCat() {
+
+//   cats.pop();
+// }
+
+// function destructivelyRemoveFirstCat() {
+
+//   cats.shift();
+// }
+
+// function appendCat(name) {
+//   const newArray = [...cats.slice(), name];
+//   return newArray;
+// }
+
+// function prependCat(name) {
+
+//   const newArray = [name, ...cats];
+//   return newArray;
+// }
+
+// function removeLastCat() {
+
+
+//   const newArray = [...cats.slice(0, cats.length -1)];
+//   return newArray;
+// }
+
+//  function removeFirstCat() {
+//   const newArray = [...cats.slice(1)];
+//   return newArray;
+//  }
